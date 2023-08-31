a0569afc558f383bb355069c40a476e8392754e5
cristian_pignatiello <cristian2937@gmail.com>
2023-08-31 12:51:56 +0200

    Added form package + create the form for the posts + corrections

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
+use DateTime;
 use App\Entity\MicroPost;
 use App\Repository\MicroPostRepository;
-use DateTime;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class MicroPostController extends AbstractController
 {
@@ -35,12 +37,41 @@ class MicroPostController extends AbstractController
         ]);
     }
 
-    #[Route('/micro/post/show/{id<\d+>}', name: 'app_micro_post_show',methods:['GET'])]
+    #[Route('/micro/post/show/{id<\d+>}', name: 'app_micro_post_show')]
     public function showOne(int $id, MicroPostRepository $microPostOne): Response
     {
         //dd($microPostOne->find($id));
         return $this->render('micro_post/show.html.twig',[
             'post'=>$microPostOne->find($id),
+            ]
+        );
+    }
+
+    #[Route('/micro/post/add', name:'app_micro_post_add',methods:['GET','POST'])]
+    public function add(Request $request, MicroPostRepository $posts): Response
+    {
+
+        $microPost = new MicroPost();
+        $form = $this->createFormBuilder($microPost)
+        ->add('title')
+        ->add('text')
+        ->add('Submit', SubmitType::class)
+        ->getForm();
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $post = $form->getData();
+            $post->setCreated(new DateTime());
+            //dd($post);
+            $posts->save($post,true);
+            
+            $this->addFlash('success','Post saved correctly');
+            return $this->redirectToRoute('app_micro_post');
+        }
+
+        return $this->renderForm('micro_post/add.html.twig',
+            [
+                'form' => $form
             ]
         );
     }
