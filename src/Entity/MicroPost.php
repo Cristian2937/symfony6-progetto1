df461f2a1c2a1f336033afd5349ef3eda8dcf807
cristian_pignatiello <cristian2937@gmail.com>
2023-09-02 14:42:38 +0200

    Added validator package for security in form fields + added security constraints + corrections

--- src/Entity/MicroPost.php
+++ src/Entity/MicroPost.php
@@ -6,19 +6,24 @@ use DateTimeInterface;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\MicroPostRepository;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: MicroPostRepository::class)]
 class MicroPost
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column]
+    #[ORM\Column(type: 'integer')]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(type: 'string',length: 255)]
+    #[Assert\NotBlank()]
+    #[Assert\Length(min: 5, max: 500, minMessage: 'The minimum chars required are {{ limit }}', maxMessage:'The maximum chars required are {{ limit }}')]
     private ?string $title = null;
 
-    #[ORM\Column(length: 500)]
+    #[ORM\Column(type: 'string',length: 500)]
+    #[Assert\NotBlank()]
+    #[Assert\Length(min: 5, max: 500, minMessage: 'The minimum characters required are 5', maxMessage:'The maximum chars required are 500')]
     private ?string $text = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
--- src/Form/MicroPostType.php
+++ src/Form/MicroPostType.php
@@ -4,16 +4,26 @@ namespace App\Form;
 
 use App\Entity\MicroPost;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotNull;
 
 class MicroPostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('text')
+            ->add('title',TextType::class,[
+                'constraints'=>[ // AGGIUNTO CONTSTRAINTS PER IL CAMPO DEL TITOLO CHE NON PUO ESSERE NULL
+                    new NotNull(message: html_entity_decode('&#33; This field cannot be null')),
+                    ]
+            ])
+            ->add('text',TextType::class,[
+                'constraints'=>[ // AGGIUNTO CONTSTRAINTS PER IL CAMPO DEL TESTO CHE NON PUO ESSERE NULL
+                    new NotNull(message: html_entity_decode('&#33; This field cannot be null')),
+                    ]
+            ])
         ;
     }
 
